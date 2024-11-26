41e307ba6f66f60cb4e66a793e17c474ee197ba7
Felipe Muller <mullerfelipe45@gmail.com>
2024-11-26 17:34:04 -0300

    test

--- global/global.js
+++ global/global.js
@@ -1,29 +1,26 @@
 function updateCartBadge() {
-  const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
-  const badge = document.getElementById("cart-badge");
+  const cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+  const badge = document.getElementById('cart-badge');
 
   console.log("Itens no carrinho:", cart); // Diagnóstico
   if (badge) {
-    badge.textContent = cart.length;
+      badge.textContent = cart.length;
   }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const navbar = document.querySelector(".navbar-geral");
-  if (navbar) {
-    updateCartBadge();
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(updateCartBadge, 10);
 });
 
-function showAutoPopup(message, duration = 3000) {
-  const popup = document.getElementById("auto-popup");
-  const messageElement = document.getElementById("popup-message");
 
+function showAutoPopup(message, duration = 3000) {
+  const popup = document.getElementById('auto-popup');
+  const messageElement = document.getElementById('popup-message');
+  
   messageElement.textContent = message;
 
-  popup.classList.remove("hidden");
+  popup.classList.remove('hidden');
 
   setTimeout(() => {
-    popup.classList.add("hidden");
+      popup.classList.add('hidden');
   }, duration);
 }
