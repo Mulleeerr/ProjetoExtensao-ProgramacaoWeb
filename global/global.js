120b5dd2a7385550a3f755d6ad794b558823f73c
Felipe Muller <mullerfelipe45@gmail.com>
2024-11-26 17:28:06 -0300

    changing updateCartBadge

--- global/global.js
+++ global/global.js
@@ -1,26 +1,25 @@
 function updateCartBadge() {
-  const cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-  const badge = document.getElementById('cart-badge');
+  const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+  const badge = document.getElementById("cart-badge");
 
   console.log("Itens no carrinho:", cart); // Diagnóstico
   if (badge) {
-      badge.textContent = cart.length;
+    badge.textContent = cart.length;
   }
 }
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(updateCartBadge, 100);
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(updateCartBadge, 10);
 });
 
-
 function showAutoPopup(message, duration = 3000) {
-  const popup = document.getElementById('auto-popup');
-  const messageElement = document.getElementById('popup-message');
-  
+  const popup = document.getElementById("auto-popup");
+  const messageElement = document.getElementById("popup-message");
+
   messageElement.textContent = message;
 
-  popup.classList.remove('hidden');
+  popup.classList.remove("hidden");
 
   setTimeout(() => {
-      popup.classList.add('hidden');
+    popup.classList.add("hidden");
   }, duration);
 }
